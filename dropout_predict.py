e7558f6777b3ae4bb4edcd14ec893e5b09c94754
randy <bruh@logiclabs.id>
2024-12-10 11:15:11 +0700

    update do predict

--- dropout_predict.py
+++ dropout_predict.py
@@ -2,27 +2,24 @@ import pandas as pd
 from joblib import load
 
 def predict_do(npm):
-
     npm=int(npm)
 
     data = _prepare_data()
 
     model = load('./dropout-predict/do-predict.joblib')
+
     data_mhs = data[data['npm'] == npm]
 
     if data_mhs.empty:
         return f"Data {npm} tidak ditemukan."
 
-    features = data_mhs[
-        ['ipk_mahasiswa', 'attendance_rate', 'total_activity_points', 'activity_count', 'angkatan_mahasiswa']].values
+    features = data_mhs[['ipk_mahasiswa', 'attendance_rate', 'total_activity_points','activity_count','angkatan_mahasiswa', 'count_kode_nilai_A', 'count_kode_nilai_B',
+          'count_kode_nilai_C', 'count_kode_nilai_D', 'count_kode_nilai_E']]
+    
+    # Get the probability of the student being in the "Drop-out" class
+    prediction = model.predict_proba(features)[:, 1] * 100  # Use [:, 1] for "Drop-out" probability
 
-    prediction = model.predict(features)
-
-    # Map the prediction to a human-readable status (assuming binary classification)
-    status_mapping = {0: 'Kemungkinan Rendah', 1: 'Drop-out (putus studi)'}
-    predicted_status = status_mapping.get(prediction[0], 'Status tidak diketahui')
-
-    return predicted_status
+    return prediction[0]
 
 def _prepare_data():
     mhs = pd.read_csv('./MHS.csv')
@@ -48,6 +45,29 @@ def _prepare_data():
     # Calculate attendance percentage per course
     krs['attendance_percentage'] = (krs['total_hadir'] / krs['total_pertemuan']) * 100
 
+
+    valid_grades = ['A', 'B', 'C', 'D', 'E']
+    krs['kode_nilai'] = krs['kode_nilai'].apply(lambda x: x if x in valid_grades else 'E')
+    # Step 2: Count occurrences of each grade per student
+    grade_counts = krs.pivot_table(
+        index='npm', 
+        columns='kode_nilai', 
+        aggfunc='size', 
+        fill_value=0
+    )
+
+    # Rename columns to indicate grade counts
+    grade_counts.columns = [f'count_kode_nilai_{grade}' for grade in grade_counts.columns]
+
+    # Step 3: Merge grade counts with main dataset
+    mhs = mhs.merge(grade_counts, on='npm', how='left')
+
+    # Fill missing counts with 0
+    mhs.fillna(0, inplace=True)
+
+    # Display the final dataset
+    # print(mhs)
+    # 
     total_sks = krs.groupby('npm')['sks_matakuliah'].sum().reset_index()
     total_sks.rename(columns={'sks_matakuliah': 'total_sks'}, inplace=True)
 
@@ -71,7 +91,5 @@ def _prepare_data():
     # data['attendance_rate'] = data['total_hadir'] / data['total_pertemuan']
     data['activity_count'] = sa.groupby('npm')['nama_kegiatan'].transform('count')
     data['activity_count'] = data['activity_count'].fillna(0)
-
-    X = data[['ipk_mahasiswa', 'attendance_rate', 'total_activity_points','activity_count','angkatan_mahasiswa']]
     
     return data
